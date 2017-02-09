291f2c140499bcc18716aa124e8c1c86a70da52a
Ricardo Junior <rjcjunior@hotmail.com>
2017-02-09 21:58:36 +0000

    User e login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
   resources :jogos
   resources :consoles
