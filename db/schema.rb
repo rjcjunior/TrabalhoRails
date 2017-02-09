e6a1f88034fac246a0b658d6052737595d8ee0ce
rjcjunior <rjcjunior@hotmail.com>
2017-02-09 16:58:03 -0200

    User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208230701) do
+ActiveRecord::Schema.define(version: 20170209185018) do
 
   create_table "consoles", force: :cascade do |t|
     t.string   "nome"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20170208230701) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "console_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nome"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
