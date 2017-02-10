29ae7e73795dbddeba206f866df1613f887210d9
Ricardo Junior <rjcjunior@hotmail.com>
2017-02-10 04:04:25 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :user_jogos
   get 'sessions/new'
-  
+   root to: "sessions#new"
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
